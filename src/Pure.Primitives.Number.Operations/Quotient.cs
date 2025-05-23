30a643afab8700ef699e0d395d70d3cf1fb60b25
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-23 18:34:20 +0300

    Added Quotient<T> primitive.

---
+++ src/Pure.Primitives.Number.Operations/Quotient.cs
@@ -0,0 +1,19 @@
+﻿using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Number.Operations;
+
+public sealed record Quotient<T> : INumber<T> where T : System.Numerics.IFloatingPoint<T>
+{
+    private readonly IEnumerable<INumber<T>> _values;
+
+    public Quotient(params INumber<T>[] values) : this(values.AsReadOnly()) { }
+
+    public Quotient(IEnumerable<INumber<T>> values)
+    {
+        _values = values;
+    }
+
+    public T Value => !_values.Any()
+        ? T.Zero
+        : _values.Select(x => x.Value).Aggregate((number1, number2) => number1 / number2);
+}
