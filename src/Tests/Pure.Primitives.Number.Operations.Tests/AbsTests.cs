52c752ee9427b12e6aceb0092845a447d3355270
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-23 17:57:09 +0300

    Added tests for Abs<T>

---
+++ src/Tests/Pure.Primitives.Number.Operations.Tests/AbsTests.cs
@@ -0,0 +1,36 @@
+﻿using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Number.Operations.Tests;
+
+public sealed record AbsTests
+{
+    [Fact]
+    public void CorrectFloatAbs()
+    {
+        const float expected = -10.3F;
+
+        INumber<float> abs = new Abs<float>(new Float(expected));
+
+        Assert.Equal(float.Abs(expected), abs.Value);
+    }
+
+    [Fact]
+    public void CorrectIntAbs()
+    {
+        const int expected = -10;
+
+        INumber<int> abs = new Abs<int>(new Int(expected));
+
+        Assert.Equal(int.Abs(expected), abs.Value);
+    }
+
+    [Fact]
+    public void CorrectDoubleAbs()
+    {
+        const double expected = -10;
+
+        INumber<double> abs = new Abs<double>(new Double(expected));
+
+        Assert.Equal(double.Abs(expected), abs.Value);
+    }
+}
