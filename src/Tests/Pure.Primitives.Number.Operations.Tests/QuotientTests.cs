92a2d79d1b225dc285a8c7bb2d59543ced794936
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-23 18:37:18 +0300

    Added tests for Quotient<T>

---
+++ src/Tests/Pure.Primitives.Number.Operations.Tests/QuotientTests.cs
@@ -0,0 +1,36 @@
+﻿using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Number.Operations.Tests;
+
+public sealed record QuotientTests
+{
+    [Fact]
+    public void DividesCorrectly()
+    {
+        const float a = 10.1F;
+        const float b = 20.2F;
+        const float c = 30.3F;
+
+        INumber<float> quotient = new Quotient<float>(new Float(a), new Float(b), new Float(c));
+
+        Assert.Equal(a / b / c, quotient.Value);
+    }
+
+    [Fact]
+    public void DividesOnZeroCorrectly()
+    {
+        const float a = 10.1F;
+        const float b = 20.2F;
+        const float c = 0;
+
+        INumber<float> quotient = new Quotient<float>(new Float(a), new Float(b), new Float(c));
+
+        Assert.Equal(float.PositiveInfinity, quotient.Value);
+    }
+
+    [Fact]
+    public void DividesEmptyCollection()
+    {
+        Assert.Equal(0, new Quotient<float>(Enumerable.Empty<INumber<float>>()).Value);
+    }
+}
