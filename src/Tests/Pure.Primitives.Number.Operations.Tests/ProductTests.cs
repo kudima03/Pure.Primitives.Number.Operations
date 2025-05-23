70be53f1c791120d018a12d252b5299b4154d944
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-23 18:29:56 +0300

    Added tests for Product<T>

---
+++ src/Tests/Pure.Primitives.Number.Operations.Tests/ProductTests.cs
@@ -0,0 +1,32 @@
+﻿using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Number.Operations.Tests;
+
+public sealed record ProductTests
+{
+    [Fact]
+    public void ComputeProduct()
+    {
+        const int a = 10;
+        const int b = 20;
+        const int c = 30;
+
+        INumber<int> product = new Product<int>(new Int(a), new Int(b), new Int(c));
+
+        Assert.Equal(a * b * c, product.Value);
+    }
+
+    [Fact]
+    public void ComputeProductFromLargeCollection()
+    {
+        Random random = new Random();
+        IEnumerable<double> numbers = Enumerable.Range(0, 10000).Select(_ => random.NextDouble()).ToArray();
+        Assert.Equal(numbers.Aggregate((x, y) => x * y), new Product<double>(numbers.Select(x => new Double(x))).Value);
+    }
+
+    [Fact]
+    public void ComputeProductFromEmptyCollection()
+    {
+        Assert.Equal(0, new Product<int>(Enumerable.Empty<INumber<int>>()).Value);
+    }
+}
