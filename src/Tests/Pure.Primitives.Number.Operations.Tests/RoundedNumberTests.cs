137f5b6622f60954a40b3a7ae8bb7bedfd3c67c9
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-23 18:53:55 +0300

    Added tests for RoundedNumber<T>

---
+++ src/Tests/Pure.Primitives.Number.Operations.Tests/RoundedNumberTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Number.Operations.Tests;
+
+public sealed record RoundedNumberTests
+{
+    [Fact]
+    public void RoundFloatCorrect()
+    {
+        const float a = 10.3F;
+
+        INumber<float> rounded = new RoundedNumber<float>(new Float(a));
+
+        Assert.Equal(float.Round(a), rounded.Value);
+    }
+
+    [Fact]
+    public void RoundDoubleCorrectly()
+    {
+        const double a = -10;
+
+        INumber<double> rounded = new RoundedNumber<double>(new Double(a));
+
+        Assert.Equal(double.Round(a), rounded.Value);
+    }
+}
