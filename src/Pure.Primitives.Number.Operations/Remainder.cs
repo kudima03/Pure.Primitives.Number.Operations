dffcd32eb1a83b78668491e8b288181c9ffdae5f
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-23 18:40:30 +0300

    Added Remainder<T> primitive.

---
+++ src/Pure.Primitives.Number.Operations/Remainder.cs
@@ -0,0 +1,19 @@
+﻿using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Number.Operations;
+
+public sealed record Remainder<T> : INumber<T> where T : System.Numerics.INumber<T>
+{
+    private readonly IEnumerable<INumber<T>> _values;
+
+    public Remainder(params INumber<T>[] values) : this(values.AsReadOnly()) { }
+
+    public Remainder(IEnumerable<INumber<T>> values)
+    {
+        _values = values;
+    }
+
+    public T Value => !_values.Any()
+        ? T.Zero
+        : _values.Select(x => x.Value).Aggregate((number1, number2) => number1 % number2);
+}
