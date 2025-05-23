215d68ea336883ec9c08169fbca7c45fd1d70c52
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-23 17:50:57 +0300

    Added Sum<T> implementation.

---
+++ src/Pure.Primitives.Number.Operations/Sum.cs
@@ -0,0 +1,19 @@
+﻿using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Number.Operations;
+
+public sealed record Sum<T> : INumber<T> where T : System.Numerics.INumber<T>
+{
+    private readonly IEnumerable<INumber<T>> _values;
+
+    public Sum(params INumber<T>[] values) : this(values.AsReadOnly()) { }
+
+    public Sum(IEnumerable<INumber<T>> values)
+    {
+        _values = values;
+    }
+
+    public T Value => !_values.Any()
+        ? T.Zero
+        : _values.Select(number => number.Value).Aggregate((number1, number2) => number1 + number2);
+}
