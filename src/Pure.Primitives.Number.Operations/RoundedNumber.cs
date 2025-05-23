9ed5b2cdd80c84abf3dc48b9c9f02a735bf4a1dc
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-23 18:51:41 +0300

    Added RoundedNumber<T> implementation.

---
+++ src/Pure.Primitives.Number.Operations/RoundedNumber.cs
@@ -0,0 +1,15 @@
+﻿using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Number.Operations;
+
+public sealed record RoundedNumber<T> : INumber<T> where T : System.Numerics.IFloatingPoint<T>
+{
+    private readonly INumber<T> _number;
+
+    public RoundedNumber(INumber<T> number)
+    {
+        _number = number;
+    }
+
+    public T Value => T.Round(_number.Value);
+}
