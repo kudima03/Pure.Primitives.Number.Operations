9299641604b29e9755c6c8ada1652ee59c193403
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-23 17:51:37 +0300

    Added tests for Sum<T>

---
+++ src/Tests/Pure.Primitives.Number.Operations.Tests/SumTests.cs
@@ -0,0 +1,29 @@
+﻿using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Number.Operations.Tests;
+public sealed record SumTests
+{
+    [Fact]
+    public void SumCorrectly()
+    {
+        const int a = 10;
+        const int b = 20;
+        const int c = 30;
+        INumber<int> sum = new Sum<int>(new Int(a), new Int(b), new Int(c));
+        Assert.Equal(a + b + c, sum.Value);
+    }
+
+    [Fact]
+    public void SumLargeDoubleCollectionCorrectly()
+    {
+        Random random = new Random();
+        IEnumerable<double> numbers = Enumerable.Range(0, 10000).Select(_ => random.NextDouble()).ToArray();
+        Assert.Equal(numbers.Sum(), new Sum<double>(numbers.Select(x => new Double(x))).Value);
+    }
+
+    [Fact]
+    public void EmptyCollection()
+    {
+        Assert.Equal(0, new Sum<int>(Enumerable.Empty<INumber<int>>()).Value);
+    }
+}
