871fee3fee6fbede47570a940cfe4ee4a6fcb521
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-25 16:31:33 +0300

    Implemented tests for LessThanCondition

---
+++ src/Tests/Pure.Primitives.Number.Operations.Tests/LessThanConditionTests.cs
@@ -0,0 +1,68 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Number.Operations.Tests;
+
+public sealed record LessThanConditionTests
+{
+    [Fact]
+    public void TakesNegativeResultOnSameValues()
+    {
+        IBool condition = new LessThanCondition<int>(new Int(10), new Int(10), new Int(10));
+        Assert.False(condition.Value);
+    }
+
+    [Fact]
+    public void TakesPositiveResultOnAscendingValues()
+    {
+        IBool condition = new LessThanCondition<int>(new Int(10), new Int(11), new Int(12));
+        Assert.True(condition.Value);
+    }
+
+    [Fact]
+    public void TakesNegativeResultOnDescendingValues()
+    {
+        IBool condition = new LessThanCondition<int>(new Int(12), new Int(11), new Int(10));
+        Assert.False(condition.Value);
+    }
+
+    [Fact]
+    public void TakesNegativeResultOnAllAscendingOneSameValue()
+    {
+        IBool condition = new LessThanCondition<int>(new Int(11), new Int(12), new Int(13), new Int(13));
+        Assert.False(condition.Value);
+    }
+
+    [Fact]
+    public void TakesNegativeResultOnAllDescendingOneSameValue()
+    {
+        IBool condition = new LessThanCondition<int>(new Int(13), new Int(12), new Int(11), new Int(11));
+        Assert.False(condition.Value);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnSingleElementInCollection()
+    {
+        IBool condition = new LessThanCondition<int>(new Int(10));
+        Assert.Throws<InvalidOperationException>(() => condition.Value);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnEmptyCollection()
+    {
+        IBool condition = new LessThanCondition<int>(Enumerable.Empty<INumber<int>>());
+        Assert.Throws<InvalidOperationException>(() => condition.Value);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<InvalidOperationException>(() => new LessThanCondition<float>(new Float(10)).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<InvalidOperationException>(() => new LessThanCondition<float>(new Float(10)).ToString());
+    }
+}
