672fff4b5d949233ad32c564a863b73a1dc02504
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-27 11:15:32 +0300

    Implemented IncrementedNumber<T>

---
+++ src/Pure.Primitives.Number.Operations/IncrementedNumber.cs
@@ -0,0 +1,25 @@
+﻿using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Number.Operations;
+
+public sealed record IncrementedNumber<T> : INumber<T> where T : System.Numerics.INumber<T>
+{
+    private readonly INumber<T> _number;
+
+    public IncrementedNumber(INumber<T> number)
+    {
+        _number = number;
+    }
+
+    T INumber<T>.Value => _number.Value + T.One;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
