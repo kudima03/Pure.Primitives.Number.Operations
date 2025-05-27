0ad1b625505bdacf51ab04a837efbf2a7f5b124c
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-27 11:13:08 +0300

    Implemented tests for DecrementedNumber<T>

---
+++ src/Tests/Pure.Primitives.Number.Operations.Tests/DecrementedNumberTests.cs
@@ -0,0 +1,48 @@
+﻿using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Number.Operations.Tests;
+
+public sealed record DecrementedNumberTests
+{
+    [Fact]
+    public void DecrementFloat()
+    {
+        const float sample = 10.3F;
+
+        INumber<float> number = new DecrementedNumber<float>(new Float(sample));
+
+        Assert.Equal(sample - 1, number.Value);
+    }
+
+    [Fact]
+    public void DecrementInt()
+    {
+        const int sample = 0;
+
+        INumber<int> number = new DecrementedNumber<int>(new Int(sample));
+
+        Assert.Equal(sample - 1, number.Value);
+    }
+
+    [Fact]
+    public void DecrementDouble()
+    {
+        const double sample = -10.763467567;
+
+        INumber<double> number = new DecrementedNumber<double>(new Double(sample));
+
+        Assert.Equal(sample - 1, number.Value);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new DecrementedNumber<float>(new Float(10)).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new DecrementedNumber<float>(new Float(10)).ToString());
+    }
+}
